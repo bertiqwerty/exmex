0a36a997f75d0608bc1237389bb961253efd98e0
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2021-06-20 18:31:52 +0200

    introduced expressions

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,7 @@ fn find_operators<T: Float>(text: &str) -> Vec<BinaryOperator<T>> {
         .collect()
 }
 
-fn priorized_indices(bin_ops: &Vec<BinaryOperator<f32>>) -> Vec<usize> {
+fn priorized_indices<T: Float>(bin_ops: &Vec<BinaryOperator<T>>) -> Vec<usize> {
     let mut indices: Vec<_> = (0..bin_ops.len()).collect();
     indices.sort_by(|i1, i2| {
         bin_ops[*i2]
@@ -53,25 +53,19 @@ fn priorized_indices(bin_ops: &Vec<BinaryOperator<f32>>) -> Vec<usize> {
     indices
 }
 
-type BoxResult<T> = Result<T, Box<dyn Error>>;
+struct CoreExpression<T: Float> {
+    numbers: Vec<T>,
+    bin_ops: Vec<BinaryOperator<T>>
+}
 
-pub fn eval(text: &str) -> BoxResult<f32> {
-    let bin_ops = find_operators::<f32>(text);
-    let mut numbers = find_numbers(text)?;
 
-    if numbers.len() == 0 || bin_ops.len() != numbers.len() - 1 {
-        bail!(
-            "Numbers/operators mismatch. {}/{}.",
-            numbers.len(),
-            bin_ops.len()
-        );
-    };
-    let indices = priorized_indices(&bin_ops);
-
+fn eval_core_expression<T: Float>(exp: &CoreExpression<T>) -> T {
+    let indices = priorized_indices(&exp.bin_ops);
+    let mut numbers = exp.numbers.clone();
     let mut num_inds = indices.clone();
     for (i, &bin_op_idx) in indices.iter().enumerate() {
         let num_idx = num_inds[i];
-        numbers[num_idx] = (bin_ops[bin_op_idx].f)(numbers[num_idx], numbers[num_idx + 1]);
+        numbers[num_idx] = (exp.bin_ops[bin_op_idx].f)(numbers[num_idx], numbers[num_idx + 1]);
         numbers.remove(num_idx + 1);
         for j in num_inds.iter_mut() {
             if *j > num_idx {
@@ -79,14 +73,46 @@ pub fn eval(text: &str) -> BoxResult<f32> {
             }
         }
     }
-    Ok(numbers[0])
+    numbers[0]
+}
+
+type BoxResult<T> = Result<T, Box<dyn Error>>;
+
+pub fn eval(text: &str) -> BoxResult<f32> {
+    let exp = CoreExpression{
+    bin_ops: find_operators::<f32>(text),
+    numbers: find_numbers(text)?
+    };
+    if exp.numbers.len() == 0 || exp.bin_ops.len() != exp.numbers.len() - 1 {
+        bail!(
+            "Numbers/operators mismatch. {}/{}.",
+            exp.numbers.len(),
+            exp.bin_ops.len()
+        );
+    };
+    Ok(eval_core_expression(&exp))
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Paran {
+    OPEN(usize),
+    CLOSE(usize)
+}
+
+fn find_parantheses(text: &str) -> Vec<Paran> {
+    text
+        .chars()
+        .enumerate()
+        .filter(|(_, c)| *c == '(' || *c == ')')
+        .map(|(i,c)| if c == '(' {Paran::OPEN(i)} else {Paran::CLOSE(i)})
+        .collect::<Vec<_>>()
 }
 
 #[cfg(test)]
 mod tests {
 
     use crate::{
-        eval, find_numbers, find_op, find_operators, make_binary_operators, priorized_indices,
+        eval, find_numbers, find_op, find_operators, make_binary_operators, priorized_indices, find_parantheses, Paran
     };
     fn assert_float_eq(f1: f32, f2: f32) {
         if (f1 - f2).abs() >= 1e-5 {
@@ -163,5 +189,18 @@ mod tests {
         assert_float_eq(eval(&"1.3+0.7*2-1/10").unwrap(), 2.6);
         assert!(eval(&"1.3+0.7**2-1/10").is_err());
         assert!(eval(&"").is_err());
+    }
+
+    #[test]
+    fn test_find_parans() {
+        assert_eq!(find_parantheses("()"), vec![Paran::OPEN(0), Paran::CLOSE(1)]);
+        assert_eq!(find_parantheses(""), vec![]);
+        assert_eq!(find_parantheses("(3*(4+5)) * (4-5)"), vec![
+            Paran::OPEN(0), 
+            Paran::OPEN(3),
+            Paran::CLOSE(7),
+            Paran::CLOSE(8),
+            Paran::OPEN(12),
+            Paran::CLOSE(16)]);
     }
 }
