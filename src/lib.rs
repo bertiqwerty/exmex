59f0e2e1250530a7f6fe3a867cfd48b761a7b4a1
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2021-08-06 15:28:54 +0200

    version bump

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/exmex/0.8.2")]
+#![doc(html_root_url = "https://docs.rs/exmex/0.8.3")]
 //! Exmex is a fast, simple, and **ex**tendable **m**athematical **ex**pression evaluator.  
 //! ```rust
 //! # use std::error::Error;
