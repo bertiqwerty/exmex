7e2467541f245f850700e27580a9afa84efbbfd7
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2021-08-28 18:19:18 +0200

    serde for owned flatex, towards #17

--- src/expression/serde.rs
+++ src/expression/serde.rs
@@ -3,7 +3,7 @@ use std::{fmt, fmt::Debug, marker::PhantomData, str::FromStr};
 use num::Float;
 use serde::{de, de::Visitor, Deserialize, Deserializer, Serialize, Serializer};
 
-use crate::{expression::deep::DeepEx, expression::flat, expression::flat::FlatEx};
+use crate::{expression::deep::DeepEx, expression::flat, expression::flat::{FlatEx, OwnedFlatEx}};
 
 impl<'de: 'a, 'a, T: Copy + Debug> Serialize for FlatEx<'a, T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -61,6 +61,62 @@ where
     }
 }
 
+impl<'de, T: Copy + Debug> Serialize for OwnedFlatEx<T> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(
+            self.unparse()
+                .map_err(|e| {
+                    serde::ser::Error::custom(format!("serialization failed - {}", e.msg))
+                })?
+                .as_str(),
+        )
+    }
+}
+
+impl<'de, T: Float + Debug + FromStr> Deserialize<'de> for OwnedFlatEx<T>
+where
+    <T as std::str::FromStr>::Err: Debug,
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_str(OwnedFlatExVisitor {
+            generic_dummy: PhantomData,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct OwnedFlatExVisitor<T> {
+    generic_dummy: PhantomData<T>,
+}
+
+impl<'de, T: Float + Debug + FromStr> Visitor<'de> for OwnedFlatExVisitor<T>
+where
+    <T as std::str::FromStr>::Err: Debug,
+{
+    type Value = OwnedFlatEx<T>;
+
+    fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "a &str that can be parsed by `exmex` crate")
+    }
+
+    fn visit_str<E>(self, unparsed: &str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        let deepex = DeepEx::from_str(unparsed);
+        match deepex {
+            Ok(d) => Ok(OwnedFlatEx::from_flatex(flat::flatten(d))),
+            Err(epe) => Err(E::custom(format!("Parse error - {}", epe.msg))),
+        }
+    }
+}
+
 #[cfg(test)]
 use serde_test::Token;
 
@@ -72,6 +128,13 @@ fn test_ser_de() {
         let serialized = serde_json::to_string(&exp).unwrap();
         let deserialized = serde_json::from_str::<FlatEx<f64>>(serialized.as_str()).unwrap();
         assert_eq!(s, format!("{}", deserialized));
+
+        let exp = OwnedFlatEx::from_flatex(exp);
+        serde_test::assert_ser_tokens(&exp, &[Token::Str(s)]);
+        let serialized = serde_json::to_string(&exp).unwrap();
+        let deserialized = serde_json::from_str::<OwnedFlatEx<f64>>(serialized.as_str()).unwrap();
+        assert_eq!(s, format!("{}", deserialized));
+
     };
 
     let test = |s, s_1| {
