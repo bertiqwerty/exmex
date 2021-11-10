dd8c13ab3b0215f89d5051e1b71d87b6ce1a2d51
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2021-11-10 16:12:58 +0100

    fixed test

--- tests/value.rs
+++ tests/value.rs
@@ -8,7 +8,7 @@ fn test_vars() -> ExResult<()> {
     let tuple = make_tuple!(i32, f64, (true, Bool), (3.4, Float), (3, Int));
     let expr = exmex::parse_val::<i32, f64>("x.1+5.3")?;
     utils::assert_float_eq_f64(expr.eval(&[tuple.clone()])?.to_float()?, 8.7);
-    let expr = exmex::parse_val_owned::<i32, f64>("-ifelse(x)+5.3")?;
+    let expr = exmex::parse_val_owned::<i32, f64>("-(x.1 if x.0 else x.2)+5.3")?;
     utils::assert_float_eq_f64(expr.eval(&[tuple.clone()])?.to_float()?, 1.9);
 
     let expr = exmex::parse_val_owned::<i64, f32>("-sin(x)+5.3")?;
