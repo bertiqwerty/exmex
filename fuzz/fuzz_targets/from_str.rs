b68dcd0200ad7d76a0ad0b337db13a01e177a6bf
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2022-01-02 23:20:57 +0100

    fixed fuzzer

--- fuzz/fuzz_targets/from_str.rs
+++ fuzz/fuzz_targets/from_str.rs
@@ -17,9 +17,5 @@ fuzz_target!(|data: &[u8]| {
     };
     if let Ok(s) = std::str::from_utf8(data) {
         let _ = FlatEx::<f64>::from_str(s);
-        let _ = FlatEx::<f64>::from_pattern(s, s);
-    }
-    if let (Ok(s1), Ok(s2)) = (std::str::from_utf8(d1), std::str::from_utf8(d2)) {
-        let _ = FlatEx::<f64>::from_pattern(s1, s2);
     }
 });
