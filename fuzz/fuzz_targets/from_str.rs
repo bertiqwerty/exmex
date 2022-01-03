f7940bf6e3d75a9234be97a4fdfa45e693fda58e
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2022-01-03 10:31:58 +0100

    removed unnecessary code

--- fuzz/fuzz_targets/from_str.rs
+++ fuzz/fuzz_targets/from_str.rs
@@ -4,17 +4,6 @@ use libfuzzer_sys::fuzz_target;
 use exmex::prelude::*;
 
 fuzz_target!(|data: &[u8]| {
-    let dummy = [0u8];
-    let split = if data.len() > 1 {
-        data.len() / 2
-    } else {
-        0
-    };
-    let (d1, d2) = if split > 0 {
-        (&data[..split], &data[split..])    
-    } else {        
-        (data, &dummy[0..1])
-    };
     if let Ok(s) = std::str::from_utf8(data) {
         let _ = FlatEx::<f64>::from_str(s);
     }
