1d18f312469fbc603d692a6d3a35c763f6bb4a67
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2022-10-03 00:04:20 +0200

    fixed fuzz target

--- fuzz/fuzz_targets/from_str.rs
+++ fuzz/fuzz_targets/from_str.rs
@@ -1,10 +1,11 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-use exmex::prelude::*;
+use exmex::{DeepEx, prelude::*};
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
-        let _ = FlatEx::<f64>::from_str(s);
+        let _ = FlatEx::<f64>::parse(s);
+        let _ = DeepEx::<f64>::parse(s);
     }
 });
