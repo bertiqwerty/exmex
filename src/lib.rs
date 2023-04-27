b1078ada1b5f11e9f8f90fdb88a417ee2226e0f6
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2023-04-27 19:54:06 +0200

    version bump

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/exmex/0.17.2")]
+#![doc(html_root_url = "https://docs.rs/exmex/0.17.3")]
 //! Exmex is an extendable mathematical expression parser and evaluator. Ease of use, flexibility, and efficient evaluations are its main design goals.
 //! Exmex can parse mathematical expressions possibly containing variables and operators. On the one hand, it comes with a list of default operators
 //! for floating point values. For differentiable default operators, Exmex can compute partial derivatives. On the other hand, users can define their
