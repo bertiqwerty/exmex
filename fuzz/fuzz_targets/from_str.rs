69df48ca7e003f1a4174bb9fa145d0c591d72353
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2024-05-11 19:45:38 +0200

    remove fuzzer warning unused result

--- fuzz/fuzz_targets/from_str.rs
+++ fuzz/fuzz_targets/from_str.rs
@@ -6,8 +6,8 @@ use exmex::{DeepEx, prelude::*};
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
         let flatex = FlatEx::<f64>::parse(s);
-        flatex.map(|x|x.to_deepex());
+        let _ = flatex.map(|x|x.to_deepex());
         let deepex = DeepEx::<f64>::parse(s);
-        deepex.map(|d|FlatEx::from_deepex(d));
+        let _ = deepex.map(|d|FlatEx::from_deepex(d));
     }
 });
